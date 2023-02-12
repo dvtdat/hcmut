72fda35b3170ef6269bd7f462d063c3e353c1cc9
dvtdat <tiendat0417@gmail.com>
2023-02-12 21:29:10 +0700

    classtring inlab init, go to sleep now

---
+++ classstring-inlab/classtring-inlab-1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void replaceString(string s, string s1, string s2)
+{
+    for (int i = s.length() - s1.length(); i >= 0; --i)
+    {
+        bool flag = true;
+        for (int j = 0; j < s1.length(); ++j)
+        {
+            if (s[i + j] != s1[j]) flag = false;
+        }
+        
+        if (flag)
+        {
+            cout << s.substr(0, i) << s2 << s.substr(i + s1.length()); return;
+        }
+    }
+
+    cout << s;
+}
+
+int main()
+{
+    string s = "Sai Gon";
+    string s1 = "Sai Gon";
+    string s2 = "fdaasfafs";
+    replaceString(s, s1, s2);
+}
