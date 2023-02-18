a0079e78f791b8c36b7c32c26d6aacd8d6883dbe
dvtdat <tiendat0417@gmail.com>
2023-02-18 10:27:03 +0700

    start using oop

--- project-midterm/knight.cpp
+++ project-midterm/knight.cpp
@@ -1,18 +1,31 @@
 #include "knight.h"
 
-void display(int healthPoint, int level, int remedy, int maidenKiss, int phoenixDown, int rescue) 
+const int inf = 2e9 + 7;
+const int maxN = 200010;
+
+struct Knights
 {
-    cout << "HP=" << healthPoint
-        << ", level=" << level
-        << ", remedy=" << remedy
-        << ", maidenkiss=" << maidenKiss
-        << ", phoenixdown=" << phoenixDown
-        << ", rescue=" << rescue << endl;
+    int healthPoint, level, remedy, maidenKiss, phoenixDown, rescue = 0;
+} knight;
+
+struct Events
+{
+    int type[maxN];
+    int size;
+} event;
+
+void display(Knights knight) 
+{
+    cout << "HP=" << knight.healthPoint
+        << ", level=" << knight.level
+        << ", remedy=" << knight.remedy
+        << ", maidenkiss=" << knight.maidenKiss
+        << ", phoenixdown=" << knight.phoenixDown
+        << ", rescue=" << knight.rescue << endl;
 }
 
-void adventureToKoopa(string file_input, int &healthPoint, int &level, int &remedy, int &maidenKiss, int &phoenixDown, int &rescue) 
+void dataInput(string file_input, Knights knight, int events[], int &numEvent)
 {
-    
     ifstream fileIn;
     fileIn.open(file_input);
 
@@ -21,9 +34,25 @@ void adventureToKoopa(string file_input, int &healthPoint, int &level, int &reme
     getline(fileIn, secondInpStr);
     getline(fileIn, thirdInpStr);
 
-    cout << firstInpStr << '\n' << secondInpStr << '\n' << thirdInpStr << '\n';
-
     stringstream ssFirst(firstInpStr);
     stringstream ssSecond(secondInpStr);
     stringstream ssThird(thirdInpStr);
+
+    ssFirst >> knight.healthPoint >> knight.level >> knight.remedy >> knight.maidenKiss >> knight.phoenixDown >> knight.rescue;
+    
+    numEvent = 0;
+    int intInp;
+    string strInp;
+
+    while (ssSecond >> intInp) events[numEvent++] = intInp;
+    while (ssThird >> strInp) cout << strInp;
+
+}
+
+void adventureToKoopa(string file_input, int &healthPoint, int &level, int &remedy, int &maidenKiss, int &phoenixDown, int &rescue) 
+{
+    int events[200000];
+    int numEvent;
+
+    dataInput(file_input, knight, events, numEvent);
 }
